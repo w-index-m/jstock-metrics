691cedec8d05bcbf5ab5f436b4937044f8ac5c41
w-index-m <107756066+w-index-m@users.noreply.github.com>
2026-02-16 23:44:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import japanize_matplotlib
+
 from datetime import datetime, timedelta
 
 st.set_page_config(layout="wide")
